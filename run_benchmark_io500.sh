b45248acea300c06dd11a098260785fad310afbf
Marco Edoardo Santimaria <marco.santimaria@edu.unito.it>
2023-03-31 18:23:10 +0200

    run bench.sh

--- run_benchmark_io500.sh
+++ run_benchmark_io500.sh
@@ -9,34 +9,36 @@ CLUSTER=broadwell
 
 for  (( NODE=1;NODE<=$MAX_NODES; NODE=$NODE*2 ))
 do    
-    for (( TASK=2;TASK<=$MAX_TASKS; TASK=$TASK*2 ))
-    do
+	for (( TASK=2;TASK<=$MAX_TASKS; TASK=$TASK*2 ))
+	do
 	echo "Running test with $NODE nodes,  and $TASK tasks per node, on cluster $CLUSTER"
-        #GENERATE temp io500.sh benchmark
-        echo "#SBATCH -o ./log.log" | cat - io500.sh > temp && mv temp temprun.sh
-        echo "#SBATCH -e ./err.log" | cat - temprun.sh > temp && mv temp temprun.sh
-        echo "#SBATCH --ntasks-per-node=$TASK" | cat - temprun.sh > temp && mv temp temprun.sh
-        echo "#SBATCH -N $NODE" | cat - temprun.sh > temp && mv temp temprun.sh
-        echo "#SBATCH -p $CLUSTER" | cat - temprun.sh >temp && mv temp temprun.sh
-        echo "#!/bin/bash" | cat - temprun.sh > temp && mv temp temprun.sh
+		#GENERATE temp io500.sh benchmark
+		echo "#SBATCH -o ./log.log" | cat - io500.sh > temp && mv temp temprun.sh
+		echo "#SBATCH -e ./err.log" | cat - temprun.sh > temp && mv temp temprun.sh
+		echo "#SBATCH --ntasks-per-node=$TASK" | cat - temprun.sh > temp && mv temp temprun.sh
+		echo "#SBATCH -N $NODE" | cat - temprun.sh > temp && mv temp temprun.sh
+		echo "#SBATCH -p $CLUSTER" | cat - temprun.sh >temp && mv temp temprun.sh
+		echo "#!/bin/bash" | cat - temprun.sh > temp && mv temp temprun.sh
 
-        #submit job and get job id
-        JOBTASK=$(sbatch temprun.sh benchmarkConfigSantimaria.ini | tr -dc '0-9' )
-	echo "Submitted job with slurm task: $JOBTASK"
-        #sleep until next job
-        while [ $(squeue -j $JOBTASK | wc -l) -ne "1" ]
-        do
-            sleep $SLEEPTIME_SECONDS
-        done
+		#submit job and get job id
+		JOBTASK=$(sbatch temprun.sh benchmarkConfigSantimaria.ini | tr -dc '0-9' )
+		echo "Submitted job with slurm task: $JOBTASK"
+		#sleep until next job
+		while [ $(squeue -j $JOBTASK | wc -l) -ne "1" ]
+		do
+			sleep $SLEEPTIME_SECONDS
+		done
 
-        #rename targz file
-        LSTMODFILE=$(ls -1t ./results | grep .tgz | head -1)
-	echo "Moving $LSTMODFILE to final place"
-        mv ./results/$LSTMODFILE ./results/test-nodes_$NODE-tasks_$TASK.tgz
-       
-    
-    done
+		#rename targz file
+		LSTMODFILE=$(ls -1t ./results | grep .tgz | head -1)
+		echo "Moving $LSTMODFILE to final place"
+		mv ./results/$LSTMODFILE ./results/test-nodes_$NODE-tasks_$TASK.tgz
+
+		echo
+		echo
+	
+	done
 done
 
-
+rm -f temprun.sh
 
