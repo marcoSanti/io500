60484247a109364b7febc6b16b05024455e61d8b
Marco Edoardo Santimaria <marco.santimaria@edu.unito.it>
2023-03-31 18:16:57 +0200

    dos2unix

--- run_benchmark_io500.sh
+++ run_benchmark_io500.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 
-#nodi: 1 2 4 8 16 32 64. 
-#core: 1 2 4 8 16 32. al primo giro faccio 2 8 32. dopo faccio girare tutto il resto.
 
-#MAX_NODES=64
-#MAX_TASKS=32
-MAX_NODES=1
-MAX_TASKS=1
+SLEEPTIME_SECONDS=10
+MAX_NODES=64
+MAX_TASKS=32
+CLUSTER=broadwell
+
 
 for  (( NODE=1;NODE<=$MAX_NODES; NODE=$NODE*2 ))
 do    
-    for (( TASK=1;TASK<=$MAX_TASKS; TASK=$TASK*4 ))
+    for (( TASK=2;TASK<=$MAX_TASKS; TASK=$TASK*2 ))
     do
-
+	echo "Running test with $NODE nodes,  and $TASK tasks per node, on cluster $CLUSTER"
         #GENERATE temp io500.sh benchmark
         echo "#SBATCH -o ./log.log" | cat - io500.sh > temp && mv temp temprun.sh
         echo "#SBATCH -e ./err.log" | cat - temprun.sh > temp && mv temp temprun.sh
         echo "#SBATCH --ntasks-per-node=$TASK" | cat - temprun.sh > temp && mv temp temprun.sh
         echo "#SBATCH -N $NODE" | cat - temprun.sh > temp && mv temp temprun.sh
-        echo "#SBATCH -p broadwell" | cat - temprun.sh >temp && mv temp temprun.sh
+        echo "#SBATCH -p $CLUSTER" | cat - temprun.sh >temp && mv temp temprun.sh
         echo "#!/bin/bash" | cat - temprun.sh > temp && mv temp temprun.sh
 
         #submit job and get job id
-        JOBTASK=$( $(sbatch temprun.sh benchmarkConfigSantimaria.ini) | tr -dc '0-9' )
+        JOBTASK=$(sbatch temprun.sh benchmarkConfigSantimaria.ini | tr -dc '0-9' )
+	echo "Submitted job with slurm task: $JOBTASK"
+        #sleep until next job
+        while [ $(squeue -j $JOBTASK | wc -l) -ne "1" ]
+        do
+            sleep $SLEEPTIME_SECONDS
+        done
 
-       # echo $JOBTASK
+        #rename targz file
+        LSTMODFILE=$(ls -1t ./results | grep .tgz | head -1)
+	echo "Moving $LSTMODFILE to final place"
+        mv ./results/$LSTMODFILE ./results/test-nodes_$NODE-tasks_$TASK.tgz
+       
     
     done
 done
 
 
 
-
-   
